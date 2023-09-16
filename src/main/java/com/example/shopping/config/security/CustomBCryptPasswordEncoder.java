188b9a86806976b91ab2770eaf7c686093dc9b34
YuYoHan <zxzz8014@naver.com>
2023-09-16 13:45:30 +0900

    [Feat] passwordEncoder 기능 구현

---
+++ src/main/java/com/example/shopping/config/security/CustomBCryptPasswordEncoder.java
@@ -0,0 +1,7 @@
+package com.example.shopping.config.security;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class CustomBCryptPasswordEncoder extends BCryptPasswordEncoder {
+
+}
