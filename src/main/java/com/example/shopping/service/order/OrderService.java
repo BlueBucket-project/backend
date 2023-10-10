a397c1339f89611b3692241aedfc6e23cfcecadd
hyeonjin-OH <mion0602@gmail.com>
2023-10-11 01:43:02 +0900

    [Feat] OrderService - 아이템 주문 서비스 추가

--- src/main/java/com/example/shopping/service/order/OrderService.java
+++ src/main/java/com/example/shopping/service/order/OrderService.java
@@ -1,15 +1,14 @@
 package com.example.shopping.service.order;
 
 import com.example.shopping.domain.order.OrderDTO;
+import com.example.shopping.domain.order.OrderItemDTO;
+import com.example.shopping.domain.order.OrderMainDTO;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public interface OrderService {
 
     //TODO - 주문이 들어갈 때 itemstatus, itemstock, memberpoint 작업필요
-    public ResponseEntity<?> orderItem(OrderDTO order);
-
-    public OrderDTO getByOrderAdmin(long id);
-
-    public OrderDTO getByOrderMember(long id);
-
+    OrderDTO orderItem(OrderMainDTO order, String adminEmail);
 }
