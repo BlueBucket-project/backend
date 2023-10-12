dabe71f039df4bb1417309e465c3c0aae9e87255
YuYoHan <zxzz8014@naver.com>
2023-10-12 11:50:58 +0900

    [Feat] 예외처리 기능 구현

--- src/main/java/com/example/shopping/exception/GlobalExceptionAdvice.java
+++ src/main/java/com/example/shopping/exception/GlobalExceptionAdvice.java
@@ -1,8 +1,14 @@
 package com.example.shopping.exception;
 
 import com.example.shopping.exception.board.BoardException;
+import com.example.shopping.exception.externalService.ExternalServiceException;
+import com.example.shopping.exception.file.FileDownloadException;
+import com.example.shopping.exception.file.FileUploadException;
 import com.example.shopping.exception.item.ItemException;
 import com.example.shopping.exception.member.UserException;
+import com.example.shopping.exception.service.OutOfStockException;
+import com.example.shopping.exception.sessionExpire.SessionExpiredException;
+import com.example.shopping.exception.validation.DataValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -35,4 +41,41 @@ public class GlobalExceptionAdvice {
     public ResponseEntity<ItemException> handleCustomException3(ItemException itemException) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(itemException);
     }
+
+    // 검증 오류 예외처리
+    @ExceptionHandler(DataValidationException.class)
+    public ResponseEntity<DataValidationException> handleDataValidationException(DataValidationException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex);
+    }
+
+    // 인증 예외처리
+    @ExceptionHandler(SessionExpiredException.class)
+    public ResponseEntity<SessionExpiredException> handleSessionExpiredException(SessionExpiredException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex);
+    }
+
+    // 파일 업로드 예외처리
+    @ExceptionHandler(FileUploadException.class)
+    public ResponseEntity<FileUploadException> handleFileUploadException(FileUploadException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex);
+    }
+
+    // 파일 다운로드 예외처리
+    @ExceptionHandler(FileDownloadException.class)
+    public ResponseEntity<FileDownloadException> handleFileDownloadException(FileDownloadException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex);
+    }
+
+    // 외부 서비스 예외처리
+    @ExceptionHandler(ExternalServiceException.class)
+    public ResponseEntity<ExternalServiceException> handleExternalServiceException(ExternalServiceException ex) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(ex);
+    }
+
+    // 서비스 로직 예외처리
+    @ExceptionHandler(OutOfStockException.class)
+    public ResponseEntity<OutOfStockException> handleOutOfStockException(OutOfStockException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex);
+    }
+
 }
