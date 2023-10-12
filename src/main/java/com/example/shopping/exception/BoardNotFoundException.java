7b965bb2937ebd12189946033d68a01657bf4636
YuYoHan <zxzz8014@naver.com>
2023-10-12 11:25:02 +0900

    [Feat] 게시글 예외처리 기능

---
+++ src/main/java/com/example/shopping/exception/BoardNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.shopping.exception;
+
+public class BoardNotFoundException extends BoardException{
+
+    public BoardNotFoundException(String message) {
+        super(message);
+    }
+}
