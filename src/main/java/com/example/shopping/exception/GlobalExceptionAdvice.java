ecfbd172f3c1212208d169685c56eae6cae9d34a
YuYoHan <zxzz8014@naver.com>
2023-10-12 11:35:23 +0900

    [Remove] UserNotFoundException 삭제

--- src/main/java/com/example/shopping/exception/GlobalExceptionAdvice.java
+++ src/main/java/com/example/shopping/exception/GlobalExceptionAdvice.java
@@ -3,7 +3,6 @@ package com.example.shopping.exception;
 import com.example.shopping.exception.board.BoardException;
 import com.example.shopping.exception.item.ItemException;
 import com.example.shopping.exception.member.UserException;
-import com.example.shopping.exception.member.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
