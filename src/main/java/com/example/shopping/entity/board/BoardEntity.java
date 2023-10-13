ec9ac00bd1565139feb7a2a1be4ede13012a195c
YuYoHan <zxzz8014@naver.com>
2023-10-13 13:02:13 +0900

    [Modify] 게시글 엔티티 수정

--- src/main/java/com/example/shopping/entity/board/BoardEntity.java
+++ src/main/java/com/example/shopping/entity/board/BoardEntity.java
@@ -31,12 +31,17 @@ public class BoardEntity extends BaseEntity {
     private MemberEntity member;
 
     // 게시판 이미지
+    // 게시글을 수정하면 게시글 속 이미지들도 함께 수정되어야 하기 때문에
+    // 여기에 적용해야 합니다. 보통 게시물을 삭제해야 이미지가 삭제되므로
+    // 게시물이 주축이기 때문에 여기에 cascade = CascadeType.ALL을 추가
+    // orphanRemoval = true도 게시글을 삭제하면
+    // 이미지 엔티티를 고아객체가 되므로 삭제되게 여기에 추가한다.
     @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
     @OrderBy("boardImgId asc")
     private List<BoardImgEntity> boardImgDTOList = new ArrayList<>();
 
     // 댓글
-    @OneToMany(mappedBy = "board", fetch = FetchType.LAZY, orphanRemoval = true)
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
     @OrderBy("commentId asc ")
     private List<CommentEntity> commentEntityList = new ArrayList<>();
 
