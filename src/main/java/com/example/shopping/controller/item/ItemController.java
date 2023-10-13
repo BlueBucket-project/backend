9b87dcefe766791b6b89634f6fe3d5a292c8ccbf
YuYoHan <zxzz8014@naver.com>
2023-10-13 14:59:33 +0900

    [Refactor] 전체 상품 보여주기 리펙토링

--- src/main/java/com/example/shopping/controller/item/ItemController.java
+++ src/main/java/com/example/shopping/controller/item/ItemController.java
@@ -36,7 +36,7 @@ public class ItemController {
 
     // 상품 등록
     @PostMapping("")
-    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @Tag(name = "item")
     @Operation(summary = "상품 등록", description = "상품을 등록하는 API입니다.")
     public ResponseEntity<?> createItem(@Validated @RequestBody ItemDTO itemDTO,
@@ -74,7 +74,7 @@ public class ItemController {
 
     // 상품 수정
     @PutMapping("/{itemId}")
-    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @Tag(name = "item")
     @Operation(summary = "상품 수정", description = "상품을 수정하는 API입니다.")
     public ResponseEntity<?> updateItem(@PathVariable Long itemId,
@@ -93,7 +93,7 @@ public class ItemController {
 
     // 상품 삭제
     @DeleteMapping("/{itemId}")
-    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @Tag(name = "item")
     @Operation(summary = "상품 삭제", description = "상품을 삭제하는 API입니다.")
     public ResponseEntity<?> deleteItem(@PathVariable Long itemId,
@@ -122,50 +122,32 @@ public class ItemController {
             String searchKeyword) {
         try {
             // 검색하지 않을 때는 모든 글을 보여준다.
+            Page<ItemDTO> items = null;
             if(searchKeyword == null) {
-                Page<ItemDTO> items = itemServiceImpl.getItems(pageable);
-                Map<String, Object> response = new HashMap<>();
-                // 현재 페이지의 아이템 목록
-                response.put("items", items.getContent());
-                // 현재 페이지 번호
-                response.put("nowPageNumber", items.getNumber());
-                // 전체 페이지 수
-                response.put("totalPage", items.getTotalPages());
-                // 한 페이지에 출력되는 데이터 개수
-                response.put("pageSize", items.getSize());
-                // 다음 페이지 존재 여부
-                response.put("hasNextPage", items.hasNext());
-                // 이전 페이지 존재 여부
-                response.put("hasPreviousPage", items.hasPrevious());
-                // 첫 번째 페이지 여부
-                response.put("isFirstPage", items.isFirst());
-                // 마지막 페이지 여부
-                response.put("isLastPage", items.isLast());
-
-                return ResponseEntity.ok().body(response);
+                items = itemServiceImpl.getItems(pageable);
             } else {
                 // 검색할 때는 검색한 것을 보여줌
-                Page<ItemDTO> searchItems = itemServiceImpl.getSearchItems(pageable, searchKeyword);
-                Map<String, Object> response = new HashMap<>();
-                // 현재 페이지의 아이템 목록
-                response.put("items", searchItems.getContent());
-                // 현재 페이지 번호
-                response.put("nowPageNumber", searchItems.getNumber());
-                // 전체 페이지 수
-                response.put("totalPage", searchItems.getTotalPages());
-                // 한 페이지에 출력되는 데이터 개수
-                response.put("pageSize", searchItems.getSize());
-                // 다음 페이지 존재 여부
-                response.put("hasNextPage", searchItems.hasNext());
-                // 이전 페이지 존재 여부
-                response.put("hasPreviousPage", searchItems.hasPrevious());
-                // 첫 번째 페이지 여부
-                response.put("isFirstPage", searchItems.isFirst());
-                // 마지막 페이지 여부
-                response.put("isLastPage", searchItems.isLast());
-
-                return ResponseEntity.ok().body(response);
+                items = itemServiceImpl.getSearchItems(pageable, searchKeyword);
             }
+            Map<String, Object> response = new HashMap<>();
+            // 현재 페이지의 아이템 목록
+            response.put("items", items.getContent());
+            // 현재 페이지 번호
+            response.put("nowPageNumber", items.getNumber());
+            // 전체 페이지 수
+            response.put("totalPage", items.getTotalPages());
+            // 한 페이지에 출력되는 데이터 개수
+            response.put("pageSize", items.getSize());
+            // 다음 페이지 존재 여부
+            response.put("hasNextPage", items.hasNext());
+            // 이전 페이지 존재 여부
+            response.put("hasPreviousPage", items.hasPrevious());
+            // 첫 번째 페이지 여부
+            response.put("isFirstPage", items.isFirst());
+            // 마지막 페이지 여부
+            response.put("isLastPage", items.isLast());
+
+            return ResponseEntity.ok().body(response);
         } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
