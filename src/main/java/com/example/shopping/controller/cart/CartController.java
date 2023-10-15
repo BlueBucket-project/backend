8051c18704e9805892a586baea92da2b29ffd408
hyeonjin-OH <mion0602@gmail.com>
2023-10-15 14:43:55 +0900

    [Comment] TestData 주석처리

--- src/main/java/com/example/shopping/controller/cart/CartController.java
+++ src/main/java/com/example/shopping/controller/cart/CartController.java
@@ -29,10 +29,10 @@ public class CartController {
     private final CartService cartService;
 
     @PostMapping(value = "")
-//    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
     @Operation(summary = "장바구니담기", description = "상품을 장바구니에 담는 API입니다.")
     public ResponseEntity<?> insertCart(@RequestBody CartMainDTO cart, BindingResult result
-                                        //, @AuthenticationPrincipal UserDetails userDetails
+                                        , @AuthenticationPrincipal UserDetails userDetails
     ) {
         CartItemDTO cartItem;
 
@@ -42,9 +42,10 @@ public class CartController {
                 return ResponseEntity.badRequest().body(result.getClass().getSimpleName());
             }
 
-//            String email = userDetails.getUsername();
-//            cartItem = cartService.addCart(cart, email);
-            cartItem = cartService.addCart(cart, "test123@test.com");
+            String email = userDetails.getUsername();
+            cartItem = cartService.addCart(cart, email);
+            //test데이터 - 배포시 삭제필요
+            //cartItem = cartService.addCart(cart, "test123@test.com");
 
         } catch (Exception e) {
             return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
@@ -54,10 +55,10 @@ public class CartController {
     }
 
     @PutMapping(value = "/item")
-    //@PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
     @Operation(summary = "장바구니물품 수정", description = "장바구니 물품 수량을 수정하는 API입니다.")
     public ResponseEntity<?> updateCart(@RequestBody CartUpdateDTO cartItem, BindingResult result
-                                    //, @AuthenticationPrincipal UserDetails userDetails
+                                    , @AuthenticationPrincipal UserDetails userDetails
     ) {
         String res;
 
@@ -67,9 +68,10 @@ public class CartController {
                 return ResponseEntity.badRequest().body(result.getClass().getSimpleName());
             }
 
-//            String email = userDetails.getUsername();
-//            res = cartService.updateCart(cartItem, email);
-            res = cartService.updateCart(cartItem, "test123@test.com");
+            String email = userDetails.getUsername();
+            res = cartService.updateCart(cartItem, email);
+            //test데이터 - 배포시 삭제필요
+            //res = cartService.updateCart(cartItem, "test123@test.com");
 
 
         } catch (CartException e) {
@@ -80,10 +82,10 @@ public class CartController {
     }
 
     @PostMapping(value = "/items")
-    //@PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
     @Operation(summary = "장바구니삭제", description = "상품을 장바구니에서 삭제하는 API입니다.")
     public ResponseEntity<?> deleteCart(@RequestBody List<CartUpdateDTO> cartItems, BindingResult result
-                                        //, @AuthenticationPrincipal UserDetails userDetails
+                                        , @AuthenticationPrincipal UserDetails userDetails
     ) {
         String res;
 
@@ -93,9 +95,10 @@ public class CartController {
                 return ResponseEntity.badRequest().body(result.getClass().getSimpleName());
             }
 
-//            String email = userDetails.getUsername();
-//            res = cartService.deleteCart(cartItems, email);
-            res = cartService.deleteCart(cartItems, "test123@test.com");
+            String email = userDetails.getUsername();
+            res = cartService.deleteCart(cartItems, email);
+            //test데이터 - 배포시 삭제필요
+            //res = cartService.deleteCart(cartItems, "test123@test.com");
 
 
         } catch (CartException e) {
@@ -106,10 +109,10 @@ public class CartController {
     }
 
     @PostMapping(value = "/orderItems")
-    //@PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
     @Operation(summary = "장바구니구매예약", description = "장바구니 상품을 구매예약하는 API입니다.")
     public ResponseEntity<?> orderCart(@RequestBody List<CartOrderDTO> cartItems, BindingResult result
-                            //, @AuthenticationPrincipal UserDetails userDetails
+                            , @AuthenticationPrincipal UserDetails userDetails
     ) {
         String res;
 
@@ -119,9 +122,10 @@ public class CartController {
                 return ResponseEntity.badRequest().body(result.getClass().getSimpleName());
             }
 
-//            String email = userDetails.getUsername();
-//            res = cartService.orderCart(cartItems, email);
-            res = cartService.orderCart(cartItems, "test123@test.com");
+            String email = userDetails.getUsername();
+            res = cartService.orderCart(cartItems, email);
+            //test데이터 - 배포시 삭제필요
+            //res = cartService.orderCart(cartItems, "test123@test.com");
 
         } catch (CartException e) {
             return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
@@ -131,10 +135,10 @@ public class CartController {
     }
 
     @PostMapping(value = "/cancelItems")
-    //@PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
     @Operation(summary = "장바구니구매예약취소", description = "장바구니 상품을 구매예약을 취소하는 API입니다.")
     public ResponseEntity<?> cancelOrderCart(@RequestBody List<CartOrderDTO> cartItems, BindingResult result
-            //, @AuthenticationPrincipal UserDetails userDetails
+                                            , @AuthenticationPrincipal UserDetails userDetails
     ) {
         String res;
 
@@ -144,9 +148,10 @@ public class CartController {
                 return ResponseEntity.badRequest().body(result.getClass().getSimpleName());
             }
 
-//            String email = userDetails.getUsername();
-//            res = cartService.orderCart(cartItems, email);
-            res = cartService.cancelCartOrder(cartItems, "test123@test.com");
+            String email = userDetails.getUsername();
+            res = cartService.orderCart(cartItems, email);
+            //test데이터 - 배포시 삭제필요
+            //res = cartService.cancelCartOrder(cartItems, "test123@test.com");
 
         } catch (CartException e) {
             return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
@@ -157,10 +162,10 @@ public class CartController {
 
 
     @GetMapping(value = "/{mbrEmail}")
-    //@PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
     @Operation(summary = "장바구니내역조회", description = "장바구니 상품을 조회하는 API입니다.")
     public ResponseEntity<?> getCartList(@PathVariable String mbrEmail
-    //                                ,@AuthenticationPrincipal UserDetails userDetails
+                                        ,@AuthenticationPrincipal UserDetails userDetails
     )    {
 
         List<CartItemDTO> items = new ArrayList<>();
