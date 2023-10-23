1257af5e558b2222c5779a0675d6f43b6940578c
hyeonjin-OH <mion0602@gmail.com>
2023-10-23 13:20:31 +0900

    [Feat] getOrders with Pages

--- src/main/java/com/example/shopping/service/order/OrderServiceImpl.java
+++ src/main/java/com/example/shopping/service/order/OrderServiceImpl.java
@@ -11,8 +11,10 @@ import com.example.shopping.repository.order.OrderItemRepository;
 import com.example.shopping.repository.order.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.*;
 import java.util.ArrayList;
 
@@ -51,6 +53,49 @@ public class OrderServiceImpl implements  OrderService{
             orderItemList.add(orderItemRepository.findByItemSeller(member.getMemberId()));
         }
         return orderItemList;
+    }
+
+    //회원 주문내역 Page로 return
+    @Override
+    public Page<OrderItemDTO> getOrdersPage(Pageable pageable, String email) {
+        //email로 order_id조회하여 판매된 내역 보여줌
+        MemberEntity member = memberRepository.findByEmail(email);
+
+        List<OrderDTO> orderList;
+        List<OrderItemDTO> orderItemList = new ArrayList<>();
+
+        //Pageable 값 셋팅 - List to Page
+        Pageable pageRequest = createPageRequestUsing(pageable.getPageNumber(), pageable.getPageSize(), pageable.getSort());
+
+        if(member.getMemberRole() == Role.ADMIN) {
+            orderList = orderRepository.findByOrderAdmin(member.getMemberId());
+            for(OrderDTO order : orderList){
+                orderItemList.add(orderItemRepository.findByOrderOrderId(order.getOrderId()));
+            }
+        }
+        else{
+            //구매자 기준 조회
+            orderList = orderRepository.findByOrderMember(member.getMemberId());
+            for(OrderDTO order : orderList) {
+                orderItemList.add(orderItemRepository.findByOrderOrderId(order.getOrderId()));
+            }
+            //판매자 기준 조회
+            orderItemList.add(orderItemRepository.findByItemSeller(member.getMemberId()));
+        }
+
+        if(orderItemList.isEmpty()){
+            throw new EntityNotFoundException("구매한 내역이 없습니다.");
+        }
+
+        int start = (int) pageRequest.getOffset();
+        int end = Math.min((start + pageRequest.getPageSize()), orderItemList.size());
+
+        List<OrderItemDTO> subOrderItem = orderItemList.subList(start, end);
+        return new PageImpl<>(subOrderItem, pageRequest, orderItemList.size());
+    }
+
+    private Pageable createPageRequestUsing(int page, int size, Sort sort) {
+        return PageRequest.of(page, size, sort);
     }
 
 }
