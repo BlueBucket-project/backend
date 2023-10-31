73ea27c1b187bfbfb12515d66924cd873a6f9d65
YuYoHan <zxzz8014@naver.com>
2023-10-31 11:06:02 +0900

    [Modify] BoardEntity 수정

--- src/main/java/com/example/shopping/entity/board/BoardEntity.java
+++ src/main/java/com/example/shopping/entity/board/BoardEntity.java
@@ -1,5 +1,9 @@
 package com.example.shopping.entity.board;
 
+import com.example.shopping.domain.board.BoardDTO;
+import com.example.shopping.domain.board.BoardImgDTO;
+import com.example.shopping.domain.board.CreateBoardDTO;
+import com.example.shopping.domain.comment.CommentDTO;
 import com.example.shopping.entity.Base.BaseEntity;
 import com.example.shopping.entity.comment.CommentEntity;
 import com.example.shopping.entity.member.MemberEntity;
@@ -7,6 +11,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -18,7 +23,8 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class BoardEntity extends BaseEntity {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "board_id")
     private Long boardId;
 
@@ -36,9 +42,9 @@ public class BoardEntity extends BaseEntity {
     // 게시물이 주축이기 때문에 여기에 cascade = CascadeType.ALL을 추가
     // orphanRemoval = true도 게시글을 삭제하면
     // 이미지 엔티티를 고아객체가 되므로 삭제되게 여기에 추가한다.
-    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
-    @OrderBy("boardImgId asc")
-    private List<BoardImgEntity> boardImgDTOList = new ArrayList<>();
+//    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
+//    @OrderBy("boardImgId asc")
+//    private List<BoardImgEntity> boardImgEntityList = new ArrayList<>();
 
     // 댓글
     @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
@@ -50,13 +56,65 @@ public class BoardEntity extends BaseEntity {
                        String title,
                        String content,
                        MemberEntity member,
-                       List<BoardImgEntity> boardImgDTOList,
+//                       List<BoardImgEntity> boardImgEntityList,
                        List<CommentEntity> commentEntityList) {
         this.boardId = boardId;
         this.title = title;
         this.content = content;
         this.member = member;
-        this.boardImgDTOList = boardImgDTOList;
+//        this.boardImgEntityList = boardImgEntityList;
         this.commentEntityList = commentEntityList;
     }
+
+    // 게시글 DTO를 엔티티로 변환
+    public static BoardEntity toBoardEntity(BoardDTO board, MemberEntity member) {
+        BoardEntity boardEntity = BoardEntity.builder()
+                .boardId(board.getBoardId())
+                .title(board.getTitle())
+                .content(board.getContent())
+                .member(member)
+                .build();
+
+        // 이미지 처리
+//        List<BoardImgDTO> boardImgDTOList = board.getBoardImgDTOList();
+
+//        for(BoardImgDTO boardImgDTO : boardImgDTOList) {
+//            // 이미지 DTO를 엔티티로 변환
+//            BoardImgEntity boardImgEntity = BoardImgEntity.toBoardImgEntity(boardImgDTO);
+//            boardEntity.boardImgEntityList.add(boardImgEntity);
+//        }
+
+        // 댓글 처리
+        List<CommentDTO> commentDTOList = board.getCommentDTOList();
+
+        for (CommentDTO commentDTO : commentDTOList) {
+            CommentEntity commentEntity = CommentEntity.toCommentEntity(commentDTO, member, boardEntity);
+            boardEntity.commentEntityList.add(commentEntity);
+        }
+        return boardEntity;
+    }
+
+    /* 비즈니스 로직 */
+    // 게시글 작성
+    public static BoardEntity createBoard(CreateBoardDTO boardDTO, MemberEntity member) {
+        return BoardEntity.builder()
+                .title(boardDTO.getTitle())
+                .content(boardDTO.getContent())
+                .member(member)
+                .build();
+    }
+
+    // 게시글 수정
+    public static BoardEntity updateBoard(CreateBoardDTO boardDTO, MemberEntity member
+//                                          List<BoardImgEntity> boardImg
+    ) {
+        return BoardEntity.builder()
+                .title(boardDTO.getTitle())
+                .content(boardDTO.getContent())
+//                .boardImgEntityList(boardImg)
+                .member(member)
+                .build();
+    }
+
+
 }
