f347bf3f08af180385baa434c1c671e04fb0d212
YuYoHan <zxzz8014@naver.com>
2023-10-31 11:09:22 +0900

    [Refactor] ItemRepository 리팩토링

--- src/main/java/com/example/shopping/repository/item/ItemRepository.java
+++ src/main/java/com/example/shopping/repository/item/ItemRepository.java
@@ -26,10 +26,10 @@ public interface ItemRepository extends JpaRepository<ItemEntity, Long> {
 
     //JPA Method로 사용할 수 없는 경우 nativeQuery를 사용해야하며 이는 sql문법대로 써야함
     @Query(value="select * from Item i where (:name is null or i.item_name like :name)" +
-            "and (:detail is null or i.item_detail가 like :detail)" +
+            "and (:detail is null or i.item_detail like :detail)" +
             "and (:startP is null or i.item_price between :startP and :endP)" +
             "and (:place is null or i.item_place like :place)" +
-            "and (:reserver is null or i.item_reserver = :reserver)"+
+            "and (:reserver is null or i.item_reserver = :reserver)" +
             "and (:status is null or i.item_sell_status like :status)", nativeQuery = true)
     List<ItemEntity> findByConditions(@Param("name") String name, @Param("detail")String detail, @Param("startP")Long startP, @Param("endP")Long endP,
                                       @Param("place")String place, @Param("reserver")String reserver, @Param("status")String status);
