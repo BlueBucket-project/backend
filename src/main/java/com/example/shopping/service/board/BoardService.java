71f1df0393c2c1a41048d15d8835df35291c6a08
YuYoHan <zxzz8014@naver.com>
2023-10-31 11:19:05 +0900

    [Feat] 게시글 작성 기능 구현

---
+++ src/main/java/com/example/shopping/service/board/BoardService.java
@@ -0,0 +1,17 @@
+package com.example.shopping.service.board;
+
+import com.example.shopping.domain.board.BoardDTO;
+import com.example.shopping.domain.board.CreateBoardDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface BoardService {
+    // 게시글 등록
+    ResponseEntity<?> saveBoard(CreateBoardDTO boardDTO,
+//                                List<MultipartFile> boardFiles,
+                                String memberEmail) throws Exception;
+
+
+}
