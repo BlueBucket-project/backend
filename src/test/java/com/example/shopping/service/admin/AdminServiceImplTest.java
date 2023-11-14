219cd251d0c2dcd5663e71e1c3949f2fe3c9df44
hyeonjin-OH <mion0602@gmail.com>
2023-11-14 15:00:39 +0900

    [Test] AdminServiceImplTest orderItem add
    예약상품 주문 및 예외처리 관련 테스트
    단위테스트로 진행

--- src/test/java/com/example/shopping/service/admin/AdminServiceImplTest.java
+++ src/test/java/com/example/shopping/service/admin/AdminServiceImplTest.java
@@ -3,9 +3,14 @@ package com.example.shopping.service.admin;
 import com.example.shopping.domain.Item.ItemDTO;
 import com.example.shopping.domain.Item.ItemSellStatus;
 import com.example.shopping.domain.member.Role;
+import com.example.shopping.domain.order.OrderDTO;
+import com.example.shopping.domain.order.OrderItemDTO;
 import com.example.shopping.domain.order.OrderMainDTO;
 import com.example.shopping.entity.item.ItemEntity;
 import com.example.shopping.entity.member.MemberEntity;
+import com.example.shopping.exception.item.ItemException;
+import com.example.shopping.exception.member.UserException;
+import com.example.shopping.exception.service.OutOfStockException;
 import com.example.shopping.repository.board.BoardRepository;
 import com.example.shopping.repository.cart.CartItemRepository;
 import com.example.shopping.repository.cart.CartRepository;
@@ -14,16 +19,19 @@ import com.example.shopping.repository.item.ItemRepository;
 import com.example.shopping.repository.member.MemberRepository;
 import com.example.shopping.repository.order.OrderItemRepository;
 import com.example.shopping.repository.order.OrderRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -56,6 +64,7 @@ public class AdminServiceImplTest {
             .email("test123@test.com")
             .nickName("유저1")
             .memberRole(Role.USER)
+            .memberPoint(0)
             .build();
 
     MemberEntity member2 = MemberEntity.builder()
@@ -65,6 +74,7 @@ public class AdminServiceImplTest {
             .email("seller123@test.com")
             .nickName("판매자")
             .memberRole(Role.USER)
+            .memberPoint(0)
             .build();
 
     MemberEntity admin = MemberEntity.builder()
@@ -86,6 +96,33 @@ public class AdminServiceImplTest {
             .itemPlace("서울시 관악구")
             .price(10000)
             .stockNumber(3)
+            .itemImgList(new ArrayList<>())
+            .itemSeller(1L)
+            .build();
+
+    ItemEntity reservedItem1 = ItemEntity.builder()
+            .itemId(1L)
+            .itemName("테스트")
+            .itemDetail("Test Detail")
+            .itemSellStatus(ItemSellStatus.RESERVED)
+            .itemReserver(member2.getEmail())
+            .itemRamount(1)
+            .itemPlace("서울시 관악구")
+            .price(10000)
+            .stockNumber(3)
+            .itemImgList(new ArrayList<>())
+            .itemSeller(1L)
+            .build();
+    ItemEntity selledItem1= ItemEntity.builder()
+            .itemId(1L)
+            .itemName("테스트")
+            .itemDetail("Test Detail")
+            .itemSellStatus(ItemSellStatus.SELL)
+            .itemReserver("")
+            .itemRamount(0)
+            .itemPlace("서울시 관악구")
+            .price(10000)
+            .stockNumber(2)
             .itemImgList(new ArrayList<>())
             .itemSeller(1L)
             .build();
@@ -119,6 +156,50 @@ public class AdminServiceImplTest {
             .itemSeller(1L)
             .build();
 
+    ItemEntity reservedItem2 = ItemEntity.builder()
+            .itemId(2L)
+            .itemName("스프링")
+            .itemDetail("스프링 테스트")
+            .itemSellStatus(ItemSellStatus.RESERVED)
+            .itemReserver(member2.getEmail())
+            .itemRamount(1)
+            .itemPlace("서울시 강남구")
+            .price(30000)
+            .stockNumber(3)
+            .itemImgList(new ArrayList<>())
+            .boardEntityList(new ArrayList<>())
+            .itemSeller(1L)
+            .build();
+
+    ItemEntity reservedOverStockItem2 = ItemEntity.builder()
+            .itemId(2L)
+            .itemName("스프링")
+            .itemDetail("스프링 테스트")
+            .itemSellStatus(ItemSellStatus.RESERVED)
+            .itemReserver(member2.getEmail())
+            .itemRamount(5)
+            .itemPlace("서울시 강남구")
+            .price(30000)
+            .stockNumber(3)
+            .itemImgList(new ArrayList<>())
+            .boardEntityList(new ArrayList<>())
+            .itemSeller(1L)
+            .build();
+
+    ItemEntity selledItem2 = ItemEntity.builder()
+            .itemId(2L)
+            .itemName("스프링")
+            .itemDetail("스프링 테스트")
+            .itemSellStatus(ItemSellStatus.SELL)
+            .itemReserver("")
+            .itemRamount(0)
+            .itemPlace("서울시 강남구")
+            .price(30000)
+            .stockNumber(2)
+            .itemImgList(new ArrayList<>())
+            .boardEntityList(new ArrayList<>())
+            .itemSeller(1L)
+            .build();
     ItemDTO savedItemDTO2 = ItemDTO.builder()
             .itemId(2L)
             .itemName("스프링")
@@ -169,23 +250,127 @@ public class AdminServiceImplTest {
         OrderMainDTO order = OrderMainDTO.builder()
                 .itemId(1L)
                 .count(1)
-                .itemReserver(member.getMemberId())
+                .itemReserver(member2.getMemberId())
                 .build();
         OrderMainDTO order2 = OrderMainDTO.builder()
                 .itemId(2L)
                 .count(1)
-                .itemReserver(member.getMemberId())
+                .itemReserver(member2.getMemberId())
+                .build();
+        OrderItemDTO orderItem1 = OrderItemDTO.builder()
+                .orderitemId(1L)
+                .orderDate(LocalDateTime.now())
+                .itemBuyer(member2.getMemberId())
+                .itemAmount(1)
+                .itemPrice(10000)
+                .itemSeller(member.getMemberId())
+                .item(savedItemDTO1)
+            .build();
+        OrderItemDTO orderItem2 = OrderItemDTO.builder()
+                .orderitemId(2L)
+                .orderDate(LocalDateTime.now())
+                .itemBuyer(member2.getMemberId())
+                .itemAmount(1)
+                .itemPrice(30000)
+                .itemSeller(member.getMemberId())
+                .item(savedItemDTO2)
+                .build();
+        List<OrderItemDTO> orderItems = new ArrayList<>();
+        orderItems.add(orderItem1);
+        orderItems.add(orderItem2);
+
+        OrderDTO savedOrder = OrderDTO.builder()
+                .orderId(1L)
+                .orderDate(LocalDateTime.now())
+                .orderAdmin(admin.getMemberId())
+                .orderMember(member2.getMemberId())
+                .orderItem(orderItems)
                 .build();
 
         List<OrderMainDTO> orders = new ArrayList<>();
         orders.add(order);
         orders.add(order2);
 
-        given(memberRepository.findById(member.getMemberId())).willReturn(Optional.ofNullable(member));
+        given(memberRepository.findById(member2.getMemberId())).willReturn(Optional.ofNullable(member2));
         given(memberRepository.findByEmail(admin.getEmail())).willReturn(admin);
 
-        given(itemRepository.findByItemId(1L)).willReturn(savedItem1);
+        given(itemRepository.findByItemId(1L)).willReturn(reservedItem1);
+        given(itemRepository.findByItemId(2L)).willReturn(reservedItem2);
+
+        given(orderRepository.save(any())).willReturn(savedOrder);
+
+        given(orderItemRepository.save(orderItem1,savedOrder)).willReturn(orderItem1);
+        given(orderItemRepository.save(orderItem2,savedOrder)).willReturn(orderItem2);
+
+        given(memberRepository.findByEmail(member2.getEmail())).willReturn(member2);
+        given(itemRepository.findById(orderItem1.getItemId())).willReturn(Optional.ofNullable(reservedItem1));
+        given(itemRepository.save(any())).willReturn(selledItem1);
+
+        given(memberRepository.findByEmail(member2.getEmail())).willReturn(member2);
+        given(itemRepository.findById(orderItem2.getItemId())).willReturn(Optional.ofNullable(reservedItem2));
+        given(itemRepository.save(any())).willReturn(selledItem2);
+
+        OrderDTO finalOrder = adminService.orderItem(orders, admin.getEmail());
+
+        Assertions.assertThat(finalOrder.getOrderId()).isEqualTo(savedOrder.getOrderId());
+        Assertions.assertThat(finalOrder.getOrderAdmin()).isEqualTo(savedOrder.getOrderAdmin());
+        Assertions.assertThat(finalOrder.getOrderItem().get(0).getItemId()).isEqualTo(savedOrder.getOrderItem().get(0).getItemId());
+        Assertions.assertThat(selledItem1.getStockNumber()).isEqualTo(2);
+        Assertions.assertThat(selledItem2.getStockNumber()).isEqualTo(2);
+
+    }
+
+    @Test
+    void 예약상품_주문_예외처리_예약물품아님() throws Exception {
+        OrderMainDTO order = OrderMainDTO.builder()
+                .itemId(1L)
+                .count(1)
+                .itemReserver(member2.getMemberId())
+                .build();
+        OrderMainDTO order2 = OrderMainDTO.builder()
+                .itemId(2L)
+                .count(1)
+                .itemReserver(member2.getMemberId())
+                .build();
+
+        List<OrderMainDTO> orders = new ArrayList<>();
+        orders.add(order);
+        orders.add(order2);
+
+        given(memberRepository.findById(member2.getMemberId())).willReturn(Optional.ofNullable(member2));
+        given(memberRepository.findByEmail(admin.getEmail())).willReturn(admin);
+
+        given(itemRepository.findByItemId(1L)).willReturn(reservedItem1);
         given(itemRepository.findByItemId(2L)).willReturn(savedItem2);
+
+        org.junit.jupiter.api.Assertions.assertThrows(ItemException.class, () ->adminService.orderItem(orders, admin.getEmail()));
+
+    }
+
+    @Test
+    void 예약상품_주문_예외처리_재고부족() throws Exception {
+        OrderMainDTO order = OrderMainDTO.builder()
+                .itemId(1L)
+                .count(1)
+                .itemReserver(member2.getMemberId())
+                .build();
+        OrderMainDTO order2 = OrderMainDTO.builder()
+                .itemId(2L)
+                .count(5)
+                .itemReserver(member2.getMemberId())
+                .build();
+
+        List<OrderMainDTO> orders = new ArrayList<>();
+        orders.add(order);
+        orders.add(order2);
+
+        given(memberRepository.findById(member2.getMemberId())).willReturn(Optional.ofNullable(member2));
+        given(memberRepository.findByEmail(admin.getEmail())).willReturn(admin);
+
+        given(itemRepository.findByItemId(1L)).willReturn(reservedItem1);
+        given(itemRepository.findByItemId(2L)).willReturn(reservedOverStockItem2);
+
+        org.junit.jupiter.api.Assertions.assertThrows(OutOfStockException.class, () ->adminService.orderItem(orders, admin.getEmail()));
 
     }
 }
