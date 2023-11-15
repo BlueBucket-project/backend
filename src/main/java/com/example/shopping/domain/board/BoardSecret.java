493fd59837f579e75ede2ac46842b007f630ebe3
YuYoHan <zxzz8014@naver.com>
2023-11-15 09:05:44 +0900

    [Modify] 오타 수정

--- src/main/java/com/example/shopping/domain/board/BoardSecret.java
+++ src/main/java/com/example/shopping/domain/board/BoardSecret.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 public enum BoardSecret {
     @Schema(description = "내글")
-    UN_ROCK,
+    UN_LOCK,
     @Schema(description = "다른 사람 글")
-    ROCK
+    LOCK
 }
