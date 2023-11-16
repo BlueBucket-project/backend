08a6137547c66568c654c394a65e3ddb4e710ca0
YuYoHan <zxzz8014@naver.com>
2023-11-16 12:28:34 +0900

    [Modify] 댓글 엔티티 casecade 수정

--- src/main/java/com/example/shopping/entity/comment/CommentEntity.java
+++ src/main/java/com/example/shopping/entity/comment/CommentEntity.java
@@ -8,13 +8,11 @@ import com.example.shopping.entity.member.MemberEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 import javax.persistence.*;
 
 @Entity(name = "comment")
 @Table
-@ToString
 @Getter
 @NoArgsConstructor
 public class CommentEntity extends BaseEntity {
@@ -31,7 +29,7 @@ public class CommentEntity extends BaseEntity {
     // 양방향으로 연관관계를 맺을 때 한 곳에서만
     // LAZY를 해도 반대쪽에서도 적용이 되므로
     // 그곳에서는 추가할 필요가 없다.
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "board_id")
     private BoardEntity board;
 
