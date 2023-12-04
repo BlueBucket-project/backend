d532bf5b32a2c07d7aaafa8bf0a77d5bf4792f3f
YuYoHan <zxzz8014@naver.com>
2023-12-04 11:22:00 +0900

    [Remove] CustomBCryptPasswordEncoder 삭제

--- src/main/java/com/example/shopping/config/security/CustomBCryptPasswordEncoder.java
+++
@@ -1,7 +0,0 @@
-package com.example.shopping.config.security;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class CustomBCryptPasswordEncoder extends BCryptPasswordEncoder {
-
-}
