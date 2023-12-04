93287ad41d1a70c9f998321bba359bd1d0e8d121
YuYoHan <zxzz8014@naver.com>
2023-12-04 11:17:55 +0900

    [Refactor] 관리자 리팩토링

--- src/main/java/com/example/shopping/service/admin/AdminServiceImpl.java
+++ src/main/java/com/example/shopping/service/admin/AdminServiceImpl.java
@@ -145,68 +145,6 @@ public class AdminServiceImpl implements AdminService {
     }
 
 
-    // 상품 전체 페이지 조회
-//    @Override
-//    @Transactional(readOnly = true)
-//    public Page<ItemDTO> superitendItem(Pageable pageable,
-//                                        UserDetails userDetails,
-//                                        ItemSellStatus itemSellStatus) {
-//        try {
-//            // userDetails에서 권한을 가져오기
-//            Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
-//
-//            // 권한이 있는지 체크
-//            if (!authorities.isEmpty()) {
-//                String role = authorities.iterator().next().getAuthority();
-//                log.info("권한 : " + role);
-//                if (role.equals("ADMIN") || role.equals("ROLE_ADMIN")) {
-//                    // 페이지 처리해서 예약된 것만 조회
-//                    Page<ItemEntity> items =
-//                            itemRepository.findByItemSellStatus(pageable, itemSellStatus);
-//                    log.info("items : {}", items);
-//
-//                    Page<ItemDTO> itemDTO = items.map(ItemDTO::toItemDTO);
-//                    for (ItemDTO item : itemDTO) {
-//                        item.setMemberNickName(memberRepository.findById(item.getItemSeller()).orElseThrow().getNickName());
-//                    }
-//                    return itemDTO;
-//                }
-//            }
-//            return null;
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//    // 상품 상세정보
-//    // 상품의 데이터를 읽어오는 트랜잭션을 읽기 전용으로 설정합니다.
-//    // 이럴 경우 JPA가 더티체킹(변경감지)를 수행하지 않아서 성능을 향상 시킬 수 있다.
-//    @Transactional(readOnly = true)
-//    @Override
-//    public ResponseEntity<ItemDTO> getItem(Long itemId, UserDetails userDetails) {
-//        try {
-//            // userDetails에서 권한을 가져오기
-//            Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
-//
-//            // 상품 조회
-//            ItemEntity findItem = itemRepository.findById(itemId)
-//                    .orElseThrow(EntityNotFoundException::new);
-//
-//            // 권한이 있는지 체크
-//            if (!authorities.isEmpty()) {
-//                String role = authorities.iterator().next().getAuthority();
-//                log.info("권한 : " + role);
-//                if (role.equals("ADMIN") || role.equals("ROLE_ADMIN")) {
-//                    ItemDTO itemDTO = ItemDTO.toItemDTO(findItem);
-//                    itemDTO.setMemberNickName(memberRepository.findById(itemDTO.getItemSeller()).orElseThrow().getNickName());
-//                    return ResponseEntity.ok().body(itemDTO);
-//                }
-//            }
-//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-//        } catch (EntityNotFoundException e) {
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-//        }
-//    }
 
     public OrderDTO orderItem(List<OrderMainDTO> orders, String adminEmail) {
 
