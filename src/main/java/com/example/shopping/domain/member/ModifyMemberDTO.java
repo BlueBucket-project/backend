9591053c403f69b01be00ab96b024fe71d34f68a
YuYoHan <zxzz8014@naver.com>
2023-12-06 11:17:48 +0900

    [Fix] 회원가입, 비밀번호 수정 정규식 에러 고치기

--- src/main/java/com/example/shopping/domain/member/ModifyMemberDTO.java
+++ src/main/java/com/example/shopping/domain/member/ModifyMemberDTO.java
@@ -19,7 +19,7 @@ public class ModifyMemberDTO {
     private String nickName;
 
     @Schema(description = "회원 비밀번호")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[0-9])(?=.*\\\\W)(?=\\\\S+$).{8,20}$", message = "비밀번호는 영문 소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8 ~20자의 비밀번호여야 합니다." )
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z[0-9]$@$!%*#?&]{8,15}", message = "비밀번호는 영문 소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8 ~20자의 비밀번호여야 합니다." )
     private String memberPw;
 
     @Schema(description = "회원 주소")
