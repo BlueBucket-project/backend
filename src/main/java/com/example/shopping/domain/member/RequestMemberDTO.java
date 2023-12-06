21e03bf6b56b2196af9340b95c772325c3633eda
YuYoHan <zxzz8014@naver.com>
2023-12-06 12:36:22 +0900

    [Fix] 회원가입 닉네임, 비밀번호 오류고치기

--- src/main/java/com/example/shopping/domain/member/RequestMemberDTO.java
+++ src/main/java/com/example/shopping/domain/member/RequestMemberDTO.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
