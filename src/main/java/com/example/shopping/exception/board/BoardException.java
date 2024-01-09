5fc5ee4eb2d226696d814c37b41190e13bb3f269
YuYoHan <zxzz8014@naver.com>
2024-01-09 18:30:00 +0900

    [Comment] BoardException 클래스 기능 설명 주석 추가

--- src/main/java/com/example/shopping/exception/board/BoardException.java
+++ src/main/java/com/example/shopping/exception/board/BoardException.java
@@ -1,5 +1,10 @@
 package com.example.shopping.exception.board;
-
+/*
+ *   writer : 유요한
+ *   work :
+ *          게시글 예외처리 때 사용
+ *   date : 2023/10/12
+ * */
 public class BoardException extends RuntimeException{
 
     public BoardException(String message) {
