d8d7d0200307ea4b874ca861638c021daa9f61d6
YuYoHan <zxzz8014@naver.com>
2024-01-10 01:32:41 +0900

    [Comment] 예외처리 관련 클래스 기능 설명 주석 추가

--- src/main/java/com/example/shopping/exception/sessionExpire/SessionExpiredException.java
+++ src/main/java/com/example/shopping/exception/sessionExpire/SessionExpiredException.java
@@ -1,6 +1,10 @@
 package com.example.shopping.exception.sessionExpire;
-
-// 인증 예외처리
+/*
+ *   writer : 유요한
+ *   work :
+ *          인증 예외처리
+ *   date : 2023/10/12
+ * */
 public class SessionExpiredException extends RuntimeException{
     public SessionExpiredException(String message) {
         super(message);
