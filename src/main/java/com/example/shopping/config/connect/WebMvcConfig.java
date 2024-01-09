94012e22c0af38ddd3a5ed230f107c40e43cc92d
YuYoHan <zxzz8014@naver.com>
2024-01-10 01:43:32 +0900

    [Comment] WebMvcConfig 클래스 기능 설명 주석 추가

--- src/main/java/com/example/shopping/config/connect/WebMvcConfig.java
+++ src/main/java/com/example/shopping/config/connect/WebMvcConfig.java
@@ -3,9 +3,15 @@ package com.example.shopping.config.connect;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+/*
+ *   writer : YuYoHan
+ *   work :
+ *          React와 SpringBoot와 서버가 다르기 때문에 연결하는 역할을 하고 있습니다.
+ *          프론트가 배포된 백엔드 코드로 개발할 수 있게 로컬과 프론트가 s3에 배포했을 때
+ *          배포 주소를 연결하고 있습니다. 이 설정을 하지 않으면 CORS 에러가 발생합니다.
+ *   date : 2023/12/04
+ * */
 
-// 리액트와 연동해서 협을 할 때
-// 이 설정을 하지 않으면 CORS에러가 발생합니다.
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
