0f64c805010b4ea311a128206fc06363623b9e2d
YuYoHan <zxzz8014@naver.com>
2024-01-25 11:52:51 +0900

    [Refactor] JwtAuthenticationEntryPoint 리팩토링

--- src/main/java/com/example/shopping/config/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/example/shopping/config/jwt/JwtAuthenticationEntryPoint.java
@@ -1,6 +1,5 @@
 package com.example.shopping.config.jwt;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -14,7 +13,7 @@ import java.io.IOException;
  *   writer : YuYoHan
  *   work :
  *          JWT를 통한 인증이 실패할 때 호출되는 역할입니다.
- *   date : 2023/10/04
+ *   date : 2024/01/17
  * */
 
 @Component
@@ -24,6 +23,6 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
                          HttpServletResponse response,
                          AuthenticationException authException)
             throws IOException, ServletException {
-        response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "인증에 실패했습니다.");
     }
 }
