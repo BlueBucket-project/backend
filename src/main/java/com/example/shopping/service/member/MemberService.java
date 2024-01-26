3ccaac28613ca51c2d458979a185a1f6eab611ad
YuYoHan <zxzz8014@naver.com>
2024-01-26 23:30:01 +0900

    [Rename] UpdateMemberDTO으로 이름 수정

--- src/main/java/com/example/shopping/service/member/MemberService.java
+++ src/main/java/com/example/shopping/service/member/MemberService.java
@@ -2,7 +2,7 @@ package com.example.shopping.service.member;
 
 import com.example.shopping.domain.member.RequestMemberDTO;
 import com.example.shopping.domain.member.ResponseMemberDTO;
-import com.example.shopping.domain.member.ModifyMemberDTO;
+import com.example.shopping.domain.member.UpdateMemberDTO;
 import org.springframework.http.ResponseEntity;
 
 public interface MemberService {
@@ -15,7 +15,7 @@ public interface MemberService {
     // 로그인
     ResponseEntity<?> login(String memberEmail, String memberPw);
     // 회원수정
-    ResponseEntity<?> updateUser(Long memberId, ModifyMemberDTO modifyMemberDTO, String memberEmail);
+    ResponseEntity<?> updateUser(Long memberId, UpdateMemberDTO updateMemberDTO, String memberEmail);
     // 이메일 체크
     boolean emailCheck(String email);
     // 닉네임 체크
