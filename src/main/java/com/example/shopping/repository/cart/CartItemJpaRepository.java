72b03416bbc00363ea459421f8d8e18578c76a59
YuYoHan <zxzz8014@naver.com>
2024-01-31 11:11:55 +0900

    [Add] 벌크 연산으로 장바구니 상태 전체 변경 기능 추가

--- src/main/java/com/example/shopping/repository/cart/CartItemJpaRepository.java
+++ src/main/java/com/example/shopping/repository/cart/CartItemJpaRepository.java
@@ -2,18 +2,21 @@ package com.example.shopping.repository.cart;
 
 import com.example.shopping.domain.cart.CartStatus;
 import com.example.shopping.entity.cart.CartItemEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
 /*
- *   writer : 오현진
+ *   writer : 오현진, 유요한
  *   work :
  *          장바구니 상품 레포지토리
  *          Spring Data JPA 방식을 사용하였고 fetch Join을 위하여
  *          JPQL을 사용했습니다.
- *   date : 2023/12/04
+ *   date : 2024/01/29
  * */
 public interface CartItemJpaRepository extends JpaRepository<CartItemEntity, Long> {
 
@@ -36,4 +39,15 @@ public interface CartItemJpaRepository extends JpaRepository<CartItemEntity, Lon
                                                    @Param("status")CartStatus status);
 
     List<CartItemEntity> findByItemItemId(Long itemId);
+
+    // 벌크 연산
+    // 데이터베이스의 부하를 줄이기 위해서 사용
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    // fetch join과 같습니다.
+    // 상품 엔티티를 한번에 가져오기 위해서 사용하고 있습니다.
+    @EntityGraph(attributePaths = "item")
+    @Query("update cartitem c set c.status = :newStatus where c.cartitemId in :cartItemIds")
+    List<CartItemEntity> updateCartItemsStatus(@Param("newStatus") CartStatus newStatus,
+                                               @Param("cartItemIds") List<Long> cartItemIds);
 }
