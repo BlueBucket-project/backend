4ec1dbeaaa08c4d563b7e80d8083abdd040907d2
YuYoHan <zxzz8014@naver.com>
2024-02-20 23:39:20 +0900

    [Test] CommentServiceImpl 테스트

--- src/main/java/com/example/shopping/domain/member/RequestMemberDTO.java
+++ src/main/java/com/example/shopping/domain/member/RequestMemberDTO.java
@@ -35,7 +35,8 @@ public class RequestMemberDTO {
     private String nickName;
 
     @Schema(description = "회원 비밀번호")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z[0-9]$@$!%*#?&]{8,20}", message = "비밀번호는 영문 소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8 ~20자의 비밀번호여야 합니다." )
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z[0-9]$@$!%*#?&]{8,20}",
+            message = "비밀번호는 영문 소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8 ~20자의 비밀번호여야 합니다." )
     private String memberPw;
 
     @Schema(description = "회원 권한")
