140f1151a77d13a0eab176312f6d76edcfeb4538
YuYoHan <zxzz8014@naver.com>
2024-02-20 23:34:34 +0900

    [Comment] CodeDTO 주석 추가

--- src/main/java/com/example/shopping/domain/admin/CodeDTO.java
+++ src/main/java/com/example/shopping/domain/admin/CodeDTO.java
@@ -4,7 +4,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-
+/*
+ *   writer : 유요한
+ *   work :
+ *          이메일 인증 때 받은 코드가 맞는지 체크하는 용도
+ *   date : 2024/01/10
+ * */
 @ToString
 @Getter
 @NoArgsConstructor
