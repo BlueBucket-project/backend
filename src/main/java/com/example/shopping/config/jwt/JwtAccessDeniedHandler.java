720607eb00a8ea40c3096abfa7b2be5e0f08f7c1
YuYoHan <zxzz8014@naver.com>
2024-02-20 23:37:32 +0900

    [Refactor] JwtAccessDeniedHandler 리팩토링

--- src/main/java/com/example/shopping/config/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/example/shopping/config/jwt/JwtAccessDeniedHandler.java
@@ -14,7 +14,6 @@ import java.io.IOException;
  *          JWT가 접근 권한이 없을 때 호출되는 역할입니다.
  *   date : 2023/11/15
  * */
-
 @Component
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
     @Override
